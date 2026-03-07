e5134c49c5743e16abe4f54a63c5164b223a53ae
keyhoney <keyhoney@users.noreply.github.com>
2026-03-07 23:37:19 +0900

    빌드 통과

--- blog/lib/mdx-components.tsx
+++ blog/lib/mdx-components.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { MDXComponents } from "mdx/types";
 import {
   TheoryBox,
@@ -26,15 +27,26 @@ import { slugify, extractTextFromNode } from "@/lib/headings";
 
 function createHeading(level: 2 | 3) {
   const Tag = `h${level}` as "h2" | "h3";
-  return ({ children, ...props }: React.ComponentProps<"h2">) => {
+  const MdxHeading = ({ children, ...props }: React.ComponentProps<"h2">) => {
     const text = extractTextFromNode(children);
     const id = text ? slugify(text) : undefined;
     return <Tag {...props} id={id}>{children}</Tag>;
   };
+  MdxHeading.displayName = level === 2 ? "MdxH2" : "MdxH3";
+  return MdxHeading;
 }
 
+function MdxImage(props: React.ImgHTMLAttributes<HTMLImageElement>) {
+  const src = typeof props.src === "string" ? props.src : "";
+  return (
+    // eslint-disable-next-line @next/next/no-img-element -- MDX 콘텐츠는 동적 src/외부 URL이 많아 next/image 대신 img 사용
+    <img {...props} src={toImageUrl(src)} alt={props.alt ?? ""} />
+  );
+}
+MdxImage.displayName = "MdxImage";
+
 export const mdxComponents: MDXComponents = {
-  img: (props) => <img {...props} src={toImageUrl(props.src ?? "")} alt={props.alt ?? ""} />,
+  img: MdxImage,
   h2: createHeading(2),
   h3: createHeading(3),
   RelatedCards,
